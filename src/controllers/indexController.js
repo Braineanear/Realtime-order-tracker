13640daafc673348a8f19a1c07c889c94c304c26
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-02-01 18:04:18 +0200

    :sparkles: Add Main Controller

---
+++ src/controllers/indexController.js
@@ -0,0 +1,18 @@
+const Menu = require('../models/menuModel.js');
+const catchAsync = require('../utils/catchAsync.js');
+const AppError = require('../utils/AppError.js');
+
+
+exports.homePage = catchAsync(async (req, res, next) => {
+    const menu = await Menu.find();
+    
+    if(!menu) {
+        return next(new AppError('There is no products to show', 400));
+    }
+
+    return res.render('home', { menu });    
+});
+
+exports.cartPage = (req, res, next) => {
+    res.render('user/cart');
+}
