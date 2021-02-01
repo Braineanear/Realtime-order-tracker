6eff9cc36681867b8a47acaaf831ba1c4434e26d
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-02-01 17:59:08 +0200

    :sparkles: Add Resources of Project JS/SASS

---
+++ src/resources/js/app.js
@@ -0,0 +1,127 @@
+import axios from 'axios';
+import Noty from 'noty';
+import initAdmin from './admin'
+import moment from 'moment'
+
+const addToCart = document.querySelectorAll('.add-to-cart');
+const cartCounter = document.querySelector('#cartCounter');
+
+const updateCart = async (item) => {
+    try {
+        const res = await axios.post('/update-cart', item);
+        cartCounter.innerText = res.data.totalQty;
+        new Noty({
+            type: 'success',
+            timeout: 1000,
+            text: 'Item added to cart',
+            progressBar: false,
+        }).show();
+    } catch(err) {
+        new Noty({
+            type: 'error',
+            timeout: 1000,
+            text: 'Something went wrong',
+            progressBar: false,
+        }).show();
+    }
+};
+
+addToCart.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        let item = JSON.parse(btn.dataset.item);
+        updateCart(item);
+    });
+});
+
+// Remove alert message after X seconds
+const alertMsg = document.querySelector('#success-alert')
+if(alertMsg) {
+    setTimeout(() => {
+        alertMsg.remove()
+    }, 2000)
+}
+
+// Change order status
+let statuses = document.querySelectorAll('.status_line')
+let hiddenInput = document.querySelector('#hiddenInput')
+let order = hiddenInput ? hiddenInput.value : null
+order = JSON.parse(order)
+let time = document.createElement('small')
+
+function updateStatus(order) {
+    statuses.forEach((status) => {
+        status.classList.remove('step-completed')
+        status.classList.remove('current')
+    })
+    let stepCompleted = true;
+    statuses.forEach((status) => {
+       let dataProp = status.dataset.status
+       if(stepCompleted) {
+            status.classList.add('step-completed')
+       }
+       if(dataProp === order.status) {
+            stepCompleted = false
+            time.innerText = moment(order.updatedAt).format('hh:mm A')
+            status.appendChild(time)
+           if(status.nextElementSibling) {
+            status.nextElementSibling.classList.add('current')
+           }
+       }
+    })
+
+}
+
+updateStatus(order);
+
+const paymentForm = document.querySelector('#payment-form');
+if(paymentForm){
+    paymentForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        document.querySelector("button").onclick = null;
+        const formData = new FormData(paymentForm);
+        
+        const res = await axios.post('/orders', {
+            phone: formData.get("phone"), 
+            address: formData.get("address")
+        });
+        new Noty({
+            type: 'success',
+            timeout: 1000,
+            text: res.data.message,
+            progressBar: false
+        }).show();
+
+        setTimeout(() => {
+            window.location.href = '/user/orders';
+        }, 1000);
+    });
+}
+
+
+// Socket
+let socket = io();
+
+// Join
+if(order) {
+    socket.emit('join', `order_${order._id}`);
+}
+
+let adminAreaPath = window.location.pathname;
+if(adminAreaPath.includes('admin')) {
+    initAdmin(socket)
+    socket.emit('join', 'adminRoom')
+}
+
+
+socket.on('orderUpdated', (data) => {
+    const updatedOrder = { ...order }
+    updatedOrder.updatedAt = moment().format()
+    updatedOrder.status = data.status
+    updateStatus(updatedOrder)
+    new Noty({
+        type: 'success',
+        timeout: 1000,
+        text: 'Order updated',
+        progressBar: false,
+    }).show();
+})
