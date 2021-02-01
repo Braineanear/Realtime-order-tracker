89f0df2e7e762434c7086d5fe081e9aed9471df7
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-02-01 18:01:38 +0200

    :sparkles: Add Menu Model

---
+++ src/models/menuModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const menuSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        minlength: 3,
+        required: [true, 'Please provide a name']
+    },
+    price: {
+        type: String,
+        required: [true, 'Please provide a price']
+    },
+    image: {
+        type: String,
+        required: [true, 'Please upload an image']
+    },
+    size: {
+        type: String,
+        enum: ['small', 'medium', 'large'],
+        default: 'small'
+    }
+});
+
+module.exports = mongoose.model('Menu', menuSchema);
