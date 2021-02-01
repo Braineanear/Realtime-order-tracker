39291f3226c186763a7f6fa5ff83a8f64d73df92
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-02-01 18:07:58 +0200

    :sparkles: Add Order Routes

---
+++ src/routes/orderRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const {createOrder} = require('../controllers/orderController.js');
+const auth = require('../middlewares/auth');
+
+const router = express.Router();
+
+router.post('/', auth, createOrder);
+
+module.exports = router;
