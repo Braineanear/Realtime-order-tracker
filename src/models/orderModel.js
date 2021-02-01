4e8c05edc8116dafa9aceea2b1e6a6a0ff6fb54a
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-02-01 18:08:47 +0200

    :sparkles: Add Order Model

---
+++ src/models/orderModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    items: {
+        type: Object,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    paymentType: {
+        type: String, 
+        required: true,
+        enum: ['cod', 'card'],
+        default: 'cod'
+    },
+    status: {
+        type: String,
+        default: 'order_placed'
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('Order', orderSchema);
