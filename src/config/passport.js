c695167554f7b469e43a62e91efe327213506799
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-02-01 18:03:07 +0200

    :sparkles: Add Passport Configurations

---
+++ src/config/passport.js
@@ -0,0 +1,34 @@
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/userModel');
+
+const passport = (passport) => {
+    passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+        // Login
+        // check if email exists
+        const user = await User.findOne({ email }).select('+password');
+        if(!user) {
+            return done(null, false, { message: 'No user with this email' });
+        }
+
+        
+        const match = await user.matchPassword(password);
+        
+        if(match) {
+            return done(null, user, { message: 'Logged in successfully' });
+        }
+        
+        return done(null, false, { message: 'Wrong username or password' });
+    }));
+
+    passport.serializeUser((user, done) => {
+        done(null, user._id);
+    });
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (err, user) => {
+            done(err, user);
+        })
+    });
+};
+
+module.exports = passport;
