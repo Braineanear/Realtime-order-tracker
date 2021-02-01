8252221583c08492b7e9199d5698c5855e2241d6
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-02-01 17:58:10 +0200

    :sparkles: Add User Routes

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const {loginPage, registerPage, register, login, logout} = require('../controllers/authController');
+const {getUserOrders, getSingleOrder} = require('../controllers/orderController');
+const guest = require('../middlewares/guest');
+const auth = require('../middlewares/auth');
+
+const router = express.Router();
+
+router.get('/login', guest, loginPage);
+router.get('/register', guest, registerPage);
+router.get('/orders', auth, getUserOrders);
+router.get('/orders/:id', auth, getSingleOrder);
+router.post('/login', login);
+router.post('/register', register);
+router.post('/logout', logout);
+
+module.exports = router;
