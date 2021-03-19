6b56db71d327f1a8ca808702ae72d7df3cb00074
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-03-19 08:00:13 +0200

    Add Menu Controller

---
+++ src/controllers/menuController.js
@@ -0,0 +1,13 @@
+const Menu = require('../models/menuModel.js');
+const catchAsync = require('../utils/catchAsync.js');
+const AppError = require('../utils/appError.js');
+
+exports.getOrders = catchAsync(async (req, res, next) => {
+  const menu = await Menu.find();
+
+  if (!menu) {
+    return next(new AppError('There is no products to show', 400));
+  }
+
+  return res.render('menu', { menu });
+});
